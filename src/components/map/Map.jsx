fae7cdd4029c80a7d70f194585e2b0fac3792c39
fisheeesh <swanphyo444@gmail.com>
2025-02-25 23:41:53 +0700

    get user current position with useGeolocation custom hook

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -5,22 +5,31 @@ import { useEffect, useState } from "react"
 import useApp from '../../hooks/useApp'
 import ChangeCenter from "./ChangeCenter"
 import DetectClick from "./DetectClick"
+import useGeoLocation from "../../hooks/useGeoLocations"
+import Button from "../button/Button"
 
 export default function Map() {
     const [searchParams] = useSearchParams()
     const [mapPosition, setMapPosition] = useState([40, 0])
-
     const mapLat = searchParams.get('lat')
     const mapLng = searchParams.get('lng')
 
+    const { isLoading: isLoadingPosition, position: geoLocationPosition, getPosition } = useGeoLocation()
     const { cities } = useApp()
 
     useEffect(() => {
         if (mapLat, mapLng) setMapPosition([mapLat, mapLng])
     }, [mapLat, mapLng])
 
+    useEffect(() => {
+        if (geoLocationPosition) setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
+    }, [geoLocationPosition])
+
     return (
         <div className={styles.mapContainer}>
+            {!geoLocationPosition && <Button type='position' onClick={getPosition}>
+                {isLoadingPosition ? 'Loading...' : 'User Your Location'}
+            </Button>}
             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
